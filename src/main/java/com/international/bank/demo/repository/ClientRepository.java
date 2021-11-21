4fd970ebe1fd02bed03e4eec6f26cc82a7827444
golu12 <shivam.kesherwani@gmail.com>
2021-11-21 17:22:05 +0100

    Update ClientRepository.java

--- src/main/java/com/international/bank/demo/repository/ClientRepository.java
+++ src/main/java/com/international/bank/demo/repository/ClientRepository.java
@@ -11,9 +11,9 @@ import java.util.List;
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 
-    @Query(value = "select ACCOUNT.PID , ACCOUNT.CUSTOMER_ID, ACCOUNT.ACCOUNT_TYPE, ACCOUNT.BALANCE, CLIENT.ID, CLIENT.ADDRESS , CLIENT.BRANCH, CLIENT.CUSTOMER_ID, CLIENT.EMAIL_ID, CLIENT.FIRST_NAME, CLIENT.LAST_NAME, CLIENT.IFSC_CODE from ACCOUNT INNER JOIN CLIENT  where ACCOUNT.CUSTOMER_ID=: custId" , nativeQuery = true)
+    @Query(value = "select  * from ACCOUNT INNER JOIN CLIENT  where ACCOUNT.CUSTOMER_ID=: custId" , nativeQuery = true)
     List<Client> getCustomerRecord(@Param("custId") int custId);
 
-    @Query(value = "select ACCOUNT.ACCOUNT_NUMBER, ACCOUNT.ACCOUNT_TYPE, ACCOUNT.BALANCE, CLIENT.ID, CLIENT.ADDRESS , CLIENT.BRANCH, CLIENT.CUSTOMER_ID, CLIENT.EMAIL_ID, CLIENT.FIRST_NAME, CLIENT.LAST_NAME, CLIENT.IFSC_CODE from ACCOUNT INNER JOIN CLIENT where ACCOUNT.ACCOUNT_TYPE= 'current' and ACCOUNT.CUSTOMER_ID= :custId", nativeQuery = true)
+    @Query(value = "select * from ACCOUNT INNER JOIN CLIENT where ACCOUNT.ACCOUNT_TYPE= 'current' and ACCOUNT.CUSTOMER_ID= :custId", nativeQuery = true)
     Client getCustomerDetails(@Param("custId") int custId);
 }
