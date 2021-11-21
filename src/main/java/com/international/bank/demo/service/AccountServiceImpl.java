78a9722b0d8f5050cee27004d446d954d616eb4e
golu12 <shivam.kesherwani@gmail.com>
2021-11-21 18:43:29 +0100

    Update AccountServiceImpl.java

--- src/main/java/com/international/bank/demo/service/AccountServiceImpl.java
+++ src/main/java/com/international/bank/demo/service/AccountServiceImpl.java
@@ -7,11 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import com.international.bank.demo.Exception.ResourceNotFoundException;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import javax.validation.constraints.NotNull;
 
 @Component
 public class AccountServiceImpl implements AccountService {
@@ -55,26 +57,7 @@ public class AccountServiceImpl implements AccountService {
         try{
         List<Client> existingAccountDetails = clientRepository.getCustomerRecord(custId);
         if(!existingAccountDetails.get(0).getAccounts().get(0).getAccountNumber().isEmpty()){
-            client = new Client();
-            account = new Account();
-            client.setId(existingAccountDetails.get(0).getId());
-            client.setCustomerId(existingAccountDetails.get(0).getCustomerId());
-            client.setFirstName(existingAccountDetails.get(0).getFirstName());
-            client.setLastName(existingAccountDetails.get(0).getLastName());
-            client.setAddress(existingAccountDetails.get(0).getAddress());
-            client.setBranch(existingAccountDetails.get(0).getBranch());
-            client.setEmailId(existingAccountDetails.get(0).getEmailId());
-            client.setIfscCode(existingAccountDetails.get(0).getIfscCode());
-            account.setAccountNumber("CAP089039458");
-            account.setAccountType("current");
-            if(initialCredit > 0){
-                account.setBalance(initialCredit);
-            }else {
-                account.setBalance(0);
-            }
-            account.setCustomerId(existingAccountDetails.get(0).getAccounts().get(0).getCustomerId());
-            account.setPid(904);
-            client.setAccounts(Collections.singletonList(account));
+	    client = createRequest(existingAccountDetails,initialCredit,account,client);
             return addSavingsAccount(client);
         }
 
@@ -103,5 +86,30 @@ public class AccountServiceImpl implements AccountService {
     		LOG.error("Error while fetching account details", e.getMessage());
     	}
         return Optional.empty();
+    }
+	
+    @NotNull
+    private Client createRequest(List<Client> existingAccountDetails, double initialCredit, Account account, Client client){
+	account = new Account();
+	client = new Client();
+	client.setId(existingAccountDetails.get(0).getId());
+		client.setCustomerId(existingAccountDetails.get(0).getCustomerId());
+		client.setFirstName(existingAccountDetails.get(0).getFirstName());
+		client.setLastName(existingAccountDetails.get(0).getLastName());
+		client.setAddress(existingAccountDetails.get(0).getAddress());
+		client.setBranch(existingAccountDetails.get(0).getBranch());
+		client.setEmailId(existingAccountDetails.get(0).getEmailId());
+		client.setIfscCode(existingAccountDetails.get(0).getIfscCode());
+		account.setAccountNumber("CAP089039458");
+		account.setAccountType("current");
+		if(initialCredit > 0){
+		    account.setBalance(initialCredit);
+		}else {
+		    account.setBalance(0);
+		}
+		account.setCustomerId(existingAccountDetails.get(0).getAccounts().get(0).getCustomerId());
+		account.setPid(904);
+		client.setAccounts(Collections.singletonList(account));
+		return client;
     }
 }
