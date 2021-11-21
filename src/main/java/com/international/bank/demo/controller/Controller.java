a28da7eeba11ecf3e5162cc431014d9562a0a174
golu12 <shivam.kesherwani@gmail.com>
2021-11-21 12:50:27 +0100

    Update Controller.java

--- src/main/java/com/international/bank/demo/controller/Controller.java
+++ src/main/java/com/international/bank/demo/controller/Controller.java
@@ -17,19 +17,35 @@ public class Controller {
 
     @Autowired
     private AccountService accountService;
-
+    
+    /**
+    * Creating First Saving Account For Customer.
+    * @Param savingAccount
+    * @return 
+    */
     @PostMapping("/accounts/saving")
     public ResponseEntity createAccount(@RequestBody CustomerRequest customerRequest){
         Optional<Client> response = accountService.addSavingsAccount(customerRequest.getClient());
         return new ResponseEntity(response, HttpStatus.OK);
     }
-
+    
+    /**
+    * Creating Current Account Based On Existing Account and Transfer Initial Credit.
+    * @Param custId
+    * @Param initialCredit
+    * @return 
+    */
     @GetMapping(path = "/accounts/current", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<TransactionResponse> addCurrentAccount(@RequestParam int custId , @RequestParam double intialCredit){
         Optional<Client> response = accountService.searchAndCreateCurrentAccount(custId, intialCredit);
         return new ResponseEntity(response, HttpStatus.OK);
     }
-
+    
+    /**
+    * Fetch Customer Info For Current Account.
+    * @Param custId
+    * @return 
+    */
     @GetMapping("accounts/customerInfo")
     public ResponseEntity getCustomerDetails(@RequestParam int custId){
         Optional<Client> response = accountService.getCustomerInformationOfNewAccount(custId);
